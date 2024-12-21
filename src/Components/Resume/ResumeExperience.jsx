5fd5652ef5f4516f8588f30b4abfdfc49e2c0daf
Md Yusuf <123129431+imyusufakhtar@users.noreply.github.com>
2024-12-21 23:44:08 +0530

    Update ResumeExperience.jsx

--- src/Components/Resume/ResumeExperience.jsx
+++ src/Components/Resume/ResumeExperience.jsx
@@ -19,7 +19,8 @@ const ResumeExperience = () => {
   }, []); // Empty dependency array ensures this effect runs only once
 
   if (!data) {
-    return <div className="loading"></div>;
+    // return <div className="loading"></div>;
+    return null;
   }
 
   return (
