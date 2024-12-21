7ae88fa2732755ee59fade67591a3b23f7309f83
Md Yusuf <123129431+imyusufakhtar@users.noreply.github.com>
2024-12-21 23:39:32 +0530

    Update HighlightSection.jsx

--- src/Components/Education/HighlightSection.jsx
+++ src/Components/Education/HighlightSection.jsx
@@ -19,7 +19,8 @@ const HighlightSection = () => {
   }, []); // Empty dependency array ensures this effect runs only once
 
   if (!data) {
-    return <div className="loading"></div>;
+    // return <div className="loading"></div>;
+    return null;
   }
 
   return (
