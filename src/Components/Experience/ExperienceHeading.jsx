c05495b54f6ae55d8d5c5b26325d9bf37c37b186
Md Yusuf <123129431+imyusufakhtar@users.noreply.github.com>
2024-12-21 23:41:25 +0530

    Update ExperienceHeading.jsx

--- src/Components/Experience/ExperienceHeading.jsx
+++ src/Components/Experience/ExperienceHeading.jsx
@@ -19,7 +19,8 @@ const ExperienceHeading = () => {
   }, []); // Empty dependency array ensures this effect runs only once
 
   if (!data) {
-    return <div className="loading"></div>;
+    // return <div className="loading"></div>;
+    return null;
   }
 
   return (
